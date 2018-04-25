1c5e0feb04bec2f92c4323b72e07cbcc162feabb
Andreas Neuhaus <zargony@zargony.com>
2018-04-25 19:28:58 +0200

    Fix node and npm commands not getting parameters on first use

--- .oh-my-zsh/custom/zargony.zsh
+++ .oh-my-zsh/custom/zargony.zsh
@@ -14,7 +14,8 @@ export PATH=$HOME/.local/bin:$PATH
 # Node.js (nvm)
 if [[ -f /usr/local/opt/nvm/nvm.sh ]]; then
 	export NVM_DIR=$HOME/.nvm
-	node() { unfunction node npm && source /usr/local/opt/nvm/nvm.sh && `whence -p node $*` }
-	npm() { unfunction node npm && source /usr/local/opt/nvm/nvm.sh && `whence -p npm $*` }
+	node() { unfunction node npm npx && source /usr/local/opt/nvm/nvm.sh && `whence -p node` $* }
+	npm() { unfunction node npm npx && source /usr/local/opt/nvm/nvm.sh && `whence -p npm` $* }
+	npx() { unfunction node npm npx && source /usr/local/opt/nvm/nvm.sh && `whence -p npx` $* }
 	nvm() { source /usr/local/opt/nvm/nvm.sh && nvm $* }
 fi
