9add97a51237ea8d9f79f9671da8afd6823ced55
Andreas Neuhaus <zargony@zargony.com>
2019-12-15 22:03:49 +0100

    Update shell configs, disable custom openssl

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,10 +2,9 @@ set -gx EDITOR vim
 set -gx LANG en_US.UTF-8
 set -gx PATH $HOME/.local/bin $PATH
 
-# Homebrew OpenSSL
-if test -d /usr/local/opt/openssl
-    set -gx OPENSSL_LIB_DIR /usr/local/opt/openssl/lib
-    set -gx OPENSSL_INCLUDE_DIR /usr/local/opt/openssl/include
+# Starship prompt - https://starship.rs/
+if test -x /usr/local/bin/starship
+    /usr/local/bin/starship init fish |source
 end
 
 # Rust - https://www.rustup.rs/
@@ -13,12 +12,13 @@ if test -x $HOME/.cargo/bin
     set -gx PATH $HOME/.cargo/bin $PATH
 end
 
-# Starship prompt - https://starship.rs/
-if test -x $HOME/.cargo/bin/starship
-    $HOME/.cargo/bin/starship init fish |source
+# Rust sccache - https://github.com/mozilla/sccache/
+if test -x /usr/local/bin/sccache
+    set -gx RUSTC_WRAPPER /usr/local/bin/sccache
 end
 
-# Rust sccache - https://github.com/mozilla/sccache/
-if test -x $HOME/.cargo/bin/sccache
-    set -gx RUSTC_WRAPPER $HOME/.cargo/bin/sccache
-end
+# Homebrew OpenSSL
+#if test -d /usr/local/opt/openssl
+#    set -gx OPENSSL_LIB_DIR /usr/local/opt/openssl/lib
+#    set -gx OPENSSL_INCLUDE_DIR /usr/local/opt/openssl/include
+#end
--- .oh-my-zsh/custom/zargony.zsh
+++ .oh-my-zsh/custom/zargony.zsh
@@ -1,18 +1,21 @@
 export EDITOR='vim'
 export PATH=$HOME/.local/bin:$PATH
 
-# Homebrew completions
-[[ -d /usr/local/Homebrew/completions/zsh ]] && fpath=(/usr/local/Homebrew/completions/zsh $fpath)
-
-# Homebrew OpenSSL
-[[ -d /usr/local/opt/openssl/lib ]] && export OPENSSL_LIB_DIR=/usr/local/opt/openssl/lib
-[[ -d /usr/local/opt/openssl/include ]] && export OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include
+# Starship prompt - https://starship.rs/
+[[ -x /usr/local/bin/starship ]] && eval "$(/usr/local/bin/starship init zsh)"
 
 # Rust - https://www.rustup.rs/
 [[ -f $HOME/.cargo/env ]] && source $HOME/.cargo/env
 
- # Rust sccache - https://github.com/mozilla/sccache/
-[[ -x $HOME/.cargo/bin/sccache ]] && export RUSTC_WRAPPER=$HOME/.cargo/bin/sccache
+# Rust sccache - https://github.com/mozilla/sccache/
+[[ -x /usr/local/bin/sccache ]] && export RUSTC_WRAPPER=/usr/local/bin/sccache
+
+# Homebrew completions
+[[ -d /usr/local/Homebrew/completions/zsh ]] && fpath=(/usr/local/Homebrew/completions/zsh $fpath)
+
+# Homebrew OpenSSL
+#[[ -d /usr/local/opt/openssl/lib ]] && export OPENSSL_LIB_DIR=/usr/local/opt/openssl/lib
+#[[ -d /usr/local/opt/openssl/include ]] && export OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include
 
 # Node.js (nvm)
 if [[ -f /usr/local/opt/nvm/nvm.sh ]]; then
