90520e8ab5ab7bb7a69ec3aee7e0c3ab77026160
Andreas Neuhaus <zargony@zargony.com>
2020-02-29 02:04:03 +0100

    Update shell configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,10 +2,13 @@ set -gx EDITOR vim
 set -gx LANG en_US.UTF-8
 set -gx PATH $HOME/.local/bin $PATH
 
-# Starship prompt - https://starship.rs/
-if test -x /usr/local/bin/starship
-    /usr/local/bin/starship init fish |source
-end
+# Homebrew OpenSSL
+#if test -d /usr/local/opt/openssl@1.1
+#    set -gx OPENSSL_DIR /usr/local/opt/openssl@1.1
+#    set -gx OPENSSL_LIB_DIR $OPENSSL_DIR/lib
+#    set -gx OPENSSL_INCLUDE_DIR $OPENSSL_DIR/include
+#    set -gx PKG_CONFIG_PATH $OPENSSL_DIR/lib/pkgconfig $PKG_CONFIG_PATH
+#end
 
 # Rust - https://www.rustup.rs/
 if test -x $HOME/.cargo/bin
@@ -17,8 +20,7 @@ if test -x /usr/local/bin/sccache
     set -gx RUSTC_WRAPPER /usr/local/bin/sccache
 end
 
-# Homebrew OpenSSL
-#if test -d /usr/local/opt/openssl@1.1
-#    set -gx OPENSSL_LIB_DIR /usr/local/opt/openssl@1.1/lib
-#    set -gx OPENSSL_INCLUDE_DIR /usr/local/opt/openssl@1.1/include
-#end
+# Starship prompt - https://starship.rs/
+if test -x /usr/local/bin/starship
+    /usr/local/bin/starship init fish | source
+end
