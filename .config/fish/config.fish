810fc143711eadb23316f29464a33069c16d274c
Andreas Neuhaus <zargony@zargony.com>
2021-12-16 21:04:19 +0100

    Remove homebrew openssl

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,14 +7,6 @@ if test -d /opt/homebrew/bin
     set -gx PATH /opt/homebrew/bin $PATH
 end
 
-# Homebrew OpenSSL
-if test -d /opt/homebrew/opt/openssl@1.1
-    set -gx OPENSSL_DIR /opt/homebrew/opt/openssl@1.1
-    set -gx OPENSSL_LIB_DIR $OPENSSL_DIR/lib
-    set -gx OPENSSL_INCLUDE_DIR $OPENSSL_DIR/include
-    set -gx PKG_CONFIG_PATH $OPENSSL_DIR/lib/pkgconfig $PKG_CONFIG_PATH
-end
-
 # Rust - https://www.rustup.rs/
 if test -x $HOME/.cargo/bin
     set -gx PATH $HOME/.cargo/bin $PATH
