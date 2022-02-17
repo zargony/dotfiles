96923958ce6a6e7dcaa63e1556b70ea8241f4549
Andreas Neuhaus <zargony@zargony.com>
2022-02-17 09:58:31 +0100

    Update cargo shortcuts

--- .config/fish/functions/cta.fish
+++ .config/fish/functions/cta.fish
@@ -1,6 +1,7 @@
 # Cargo test all
 function cta --description 'cargo fmt, clippy and test all shorthand'
     cargo fmt --all -- --check $argv && \
-    cargo clippy --workspace --all-targets --all-features -- -Dwarnings -Drust-2018-idioms -Adeprecated $argv && \
+    #cargo clippy --workspace --all-targets --all-features -- --deny warnings --allow deprecated $argv && \
+    cargo clippy --workspace --all-targets --all-features -- $argv && \
     cargo test --workspace --all-features $argv
 end
