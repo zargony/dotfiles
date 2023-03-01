c737a9a7a59e772a98cb29aa71bc12a52117657d
Andreas Neuhaus <git@zargony.com>
2023-03-01 12:38:11 +0100

    Update cta alias

--- .config/fish/functions/cta.fish
+++ .config/fish/functions/cta.fish
@@ -2,5 +2,5 @@ function cta --description 'cargo fmt, clippy and test all shorthand'
     cargo fmt --all -- --check $argv && \
     #cargo clippy --workspace --all-targets --all-features -- --deny warnings --allow deprecated $argv && \
     cargo clippy --workspace --all-targets --all-features -- $argv && \
-    cargo test --workspace --all-features $argv
+    cargo test --workspace --all-targets --all-features $argv
 end
